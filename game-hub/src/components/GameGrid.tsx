bbb041e175f3cacefbbd2d52c82ac6e08210d06a
Artem <artem@Playys.playys>
2023-05-21 22:29:21 +0300

    request hook to get a fetch

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -2,26 +2,11 @@ import React, { useEffect, useState } from 'react'
 import {Text} from '@chakra-ui/react'
 import apiClient from '../services/api-client'
 import { chakra } from '@chakra-ui/react'
+import useGames from '../hooks/useGames'
+import {Game} from '../hooks/useGames'
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([])
-  const [error, setError] = useState([])
-
-  interface Game {
-    id: number,
-    name: string
-  }
-
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[]
-  }
-
-  useEffect(() => {
-    apiClient.get<FetchGamesResponse>('/games').then(res => setGames(res.data.results))
-    .catch(err => setError(err.message))
-  })
-
+  const {games, error} = useGames()
   return (
     <>
     {error && <Text>{error}</Text>}
