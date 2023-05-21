5dcb01ad677f0f3851d9e2d4cbdbeb942023dbc2
Artem <artem@Playys.playys>
2023-05-21 20:16:41 +0300

    build navBar

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,5 +1,6 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   return(
@@ -7,7 +8,7 @@ function App() {
       base: `"nav" "main"`,
       lg: `"nav nav" "aside main"`
     }}>
-      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      <GridItem area='nav'><NavBar></NavBar></GridItem>
       <Show above='lg'>
       <GridItem area='aside' bg='gold'>Aside</GridItem>
       </Show>
