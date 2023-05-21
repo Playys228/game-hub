98d67d40439537301205014775c7e946f8c2973c
Artem <artem@Playys.playys>
2023-05-21 20:35:07 +0300

    switching color

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -10,9 +10,9 @@ function App() {
     }}>
       <GridItem area='nav'><NavBar></NavBar></GridItem>
       <Show above='lg'>
-      <GridItem area='aside' bg='gold'>Aside</GridItem>
+      <GridItem area='aside'>Aside</GridItem>
       </Show>
-      <GridItem area='main' bg='dodgerblue'>main</GridItem>
+      <GridItem area='main'>main</GridItem>
     </Grid>
   )
 }
--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from '../assets/logo.png'
+import ColorModeSwitch from './ColorModeSwitch'
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize={'30px'} margin={'3px'}></Image>
-      <Text>NavBar</Text>
+    <HStack justifyContent='space-between' padding='10px'>
+      <Image src={logo} boxSize={'50px'} margin={'3px'}></Image>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   )
 }
