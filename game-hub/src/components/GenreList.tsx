a21f28c422f0227d30b8237d694e42745d1af9d4
Artem <artem@Playys.playys>
2023-05-22 17:58:32 +0300

    generic data fetching useEffect hook

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -1,13 +1,14 @@
+import { List } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
-    </ul>
+    </List>
   );
 };
 
