37f3ac909dcd6fa36762e05ef61bf335a8702825
Artem <artem@Playys.playys>
2023-05-23 13:32:09 +0300

    handle game with no image

--- game-hub/src/services/image-url.ts
+++ game-hub/src/services/image-url.ts
@@ -1,5 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return ''
+  if (!url) return 'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg'
   const target = 'media/'
   const index = url.indexOf(target) + target.length
   const newUrl = url.slice(0, index) + 'crop/600/400/' + url.slice(index)
