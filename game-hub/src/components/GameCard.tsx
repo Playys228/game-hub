31e0e58652c7edf7123f9003c4c373bdb4a6345d
Artem <artem@Playys.playys>
2023-05-23 15:48:56 +0300

    clean up game cards

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -14,12 +14,14 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map(({ platform }) => platform)}
           ></PlatformIconList>
           <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Heading fontSize="2xl">{game.name}</Heading>
         </HStack>
       </CardBody>
     </Card>
